cb76577cad508aa73d181d1b42ed29216bcedaf1
= <=>
2024-03-28 01:19:41 +0100

    Commit empty

--- src/main/java/tigerteam/seminaire/repository/RecevoirRepository.java
+++
@@ -1,9 +0,0 @@
-//package tigerteam.seminaire.repository;
-//
-//import org.springframework.data.jpa.repository.JpaRepository;
-//
-//import tigerteam.seminaire.model.Recevoir;
-//
-//public interface RecevoirRepository extends JpaRepository<Recevoir, Integer>{
-//
-//}
